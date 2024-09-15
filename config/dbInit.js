1700aa307d6192461654565337e190eb07b350b9
juaone1 <johnvirgil06@gmail.com>
2024-09-15 20:03:17 +0800

    added refresh_token colum in db init

--- config/dbInit.js
+++ config/dbInit.js
@@ -5,7 +5,8 @@ const dbInit = async () => {
     CREATE TABLE IF NOT EXISTS users (
       id SERIAL PRIMARY KEY,
       email VARCHAR(255) UNIQUE NOT NULL,
-      password VARCHAR(255) NOT NULL
+      password VARCHAR(255) NOT NULL,
+      refresh_token VARCHAR(255)
     );
   `);
 
